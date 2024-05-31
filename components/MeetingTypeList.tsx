7967b7dc8ee9058b89cdbceec5aae04baf0a17ac
ahmettnur <Ahmednuur0079@gmail.com>
2024-05-31 10:36:46 +0300

    done

--- components/MeetingTypeList.tsx
+++ components/MeetingTypeList.tsx
@@ -107,6 +107,7 @@ const MeetingTypeList = () => {
           isOpen={meetingState === 'isScheduleMeeting'}
           onClose={() => setMeetingState(undefined)}
           title="Create Meeting"
+          buttonText="create meeting"
           handleClick={createMeeting}
         >
           <div className="flex flex-col gap-2.5">
